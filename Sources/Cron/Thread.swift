189fdd6a79da456b86bc89814e7b2ac91638e0f6
zig <zig1375@gmail.com>
2018-02-12 15:52:00 +0300

    Fi

---
+++ Sources/Cron/Thread.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+#if os(Linux)
+typealias zThread = Thread;
+#elseif os(OSX)
+class zThread: NSObject
+{
+    private var thread: Thread?
+    private var callback: (() -> Void)?;
+
+    init(block : @escaping(() -> Void)) {
+        super.init()
+        self.callback = block;
+
+        self.thread = Thread(target: self, selector: #selector(zThread.invoke), object: nil);
+    }
+
+    func start() {
+        self.thread?.start();
+    }
+
+    func invoke() {
+        self.callback?()
+
+        // Discard callback and timer.
+        self.callback = nil
+        self.thread = nil
+    }
+}
+#endif
