678931595fc931ba12ccf8f76e99c43ab9e55bd6
zig <zig1375@gmail.com>
2018-09-21 12:18:49 +0300

    test

--- Sources/Cron/CronJob.swift
+++ Sources/Cron/CronJob.swift
@@ -1,6 +1,6 @@
 import Dispatch
 
-public struct CronJob {
+public class CronJob {
     public let pattern: DatePattern
     let job: () -> Void
     let queue: DispatchQueue
@@ -22,7 +22,7 @@ public struct CronJob {
         start()
     }
 
-    mutating public func start() {
+    public func start() {
         guard let next = pattern.next()?.date else {
             print("No next execution date could be determined")
             return
@@ -31,7 +31,7 @@ public struct CronJob {
         self.nextTime = Int(next.timeIntervalSince1970);
         let interval = next.timeIntervalSinceNow
         DispatchQueue.global().asyncAfter(deadline: .now() + interval) { () -> () in
-            if (self.nextTime <= Int(next.timeIntervalSince1970)) {
+            if (self.nextTime <= Int(Date().timeIntervalSince1970)) {
                 self.job()
             }
 
